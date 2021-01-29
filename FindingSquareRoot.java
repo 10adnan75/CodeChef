52b7e1926713a06d151ac41341724185596e0cd2
Adnan Shaikh <52044177+10adnan75@users.noreply.github.com>
2021-01-29 12:50:20 +0530

    Update FindingSquareRoot.java

--- FindingSquareRoot.java
+++ FindingSquareRoot.java
@@ -7,13 +7,22 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class FindingSquareRoot
 {
+    public static int makeSquareRoot (int num) {
+        int sqrt = 0;
+        for (int i = 0; i*i <= num; i++) {
+            sqrt = i;
+        }
+        return sqrt;
+    }
+    
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt();
 		while (n-- > 0) {
 		    int num = input.nextInt();
-		    System.out.println((int) Math.sqrt(num));
+		    int sqrt = makeSquareRoot(num);
+		    System.out.println(sqrt);
 		}
 	}
 }
